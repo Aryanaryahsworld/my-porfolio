efea561c9b87e5b7f4009c53eda35277f8ff46c5
Aryan <111681422+Aryanaryahsworld@users.noreply.github.com>
2024-08-17 18:35:24 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -109,14 +109,14 @@ export default function Intro() {
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
-        <a
+{/*         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="http://www.linkedin.com/in/aryan-s-43299c99/"
           target="_blank"
         >
           <BsLinkedin />
         </a>
-
+ */}
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://github.com/Aryanaryahsworld/"
