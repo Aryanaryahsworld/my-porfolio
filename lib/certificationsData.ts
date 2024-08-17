4c9df037e1ed85fe189f502ace36dc2b0cdb7f0c
Aryan <111681422+Aryanaryahsworld@users.noreply.github.com>
2024-08-17 11:54:24 +0530

    Update certificationsData.ts

--- lib/certificationsData.ts
+++ lib/certificationsData.ts
@@ -1,12 +1,10 @@
-
-
+// lib/certificationsData.ts
 export type Certification = {
   title: string;
   institution: string;
   date: string;
   description: string;
   pdfUrl: string;
-  
 };
 
 export const certificationsData: Certification[] = [
@@ -15,20 +13,20 @@ export const certificationsData: Certification[] = [
     institution: "American Academy of Project Management, USA (FAAPM)",
     date: "Date of Certification",
     description: "This certification was awarded by the American Academy of Project Management, recognizing expertise in Data Science.",
-    pdfUrl: "pdf/Aryan-Resume.pdf", // This uses the imported PDF file
+    pdfUrl: "pdf/Aryan-Resume.pdf",
   },
   {
     title: "Machine Learning",
     institution: "Stanford University, Coursera",
     date: "Nov 2021",
     description: "Completed the Machine Learning course offered by Stanford University on Coursera.",
-    pdfUrl: "", // Replace with the actual path or keep this as a placeholder
+    pdfUrl: "pdf/Coursera Certificates-1.pdf",
   },
   {
     title: "Cyber Security to Everyone",
     institution: "University of Maryland, Coursera",
     date: "Nov 2021",
     description: "Completed the Cyber Security to Everyone course offered by the University of Maryland on Coursera.",
-    pdfUrl: "", // Replace with the actual path or keep this as a placeholder
-  },
+    pdfUrl: "pdf/Coursera Certificates-2.pdf",
+  },
 ];
