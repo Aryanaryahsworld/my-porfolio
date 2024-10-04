7cc75929ab887e91d4ca9ad0d1b753c596f76476
Aryan <111681422+Aryanaryahsworld@users.noreply.github.com>
2024-10-04 22:38:49 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Aryan | Portfolio",
-  description: "Aryan is a Data Analyst..",
+  description: "Aryan is a Financial Analyst..",
 };
 
 export default function RootLayout({
