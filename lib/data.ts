9f39a219de3826062c5cd10a1a360167436b003d
Aryan <111681422+Aryanaryahsworld@users.noreply.github.com>
2024-10-04 23:18:59 +0530

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -8,7 +8,7 @@ import Budgety from "@/public/Budgety.png";
 import Tours from "@/public/tours.png";
 import ChatAnalyzer from "@/public/diesesepred.jpg";
 import { SiPython, SiR, SiMysql, SiMicrosoft, SiFlask, SiDjango, SiPostgresql, SiMongodb, SiOracle,  SiGit, SiGithub, SiPandas, SiNumpy, SiApacheairflow, SiSnowflake, SiTableau, SiPowerbi, SiMicrosoftaccess,  SiPycharm, SiJupyter, SiVisualstudiocode, SiJira  } from 'react-icons/si';
-import { SiCodeforces, SiGithub, SiLinkedin } from "react-icons/si";
+import { SiCodeforces,  SiLinkedin } from "react-icons/si";
 import { SiCodechef } from "react-icons/si";
 import { SiLeetcode } from "react-icons/si";
 import { SiGeeksforgeeks } from "react-icons/si";
