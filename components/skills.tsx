90eb45b2f0ec706390435a8e62bc91249a02d509
Aryan <111681422+Aryanaryahsworld@users.noreply.github.com>
2024-10-04 22:57:53 +0530

    Update skills.tsx

--- components/skills.tsx
+++ components/skills.tsx
@@ -30,31 +30,40 @@ export default function Skills() {
       ref={ref}
       className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
     >
-      <SectionHeading>My skills</SectionHeading>
-      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-        {skillsData.map((skill, index) => (
-          <motion.li
-            key={index}
-            className="relative bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80 group overflow-hidden"
-            onMouseEnter={() => setHoveredSkill(skill.name)}
-            onMouseLeave={() => setHoveredSkill(null)}
-            variants={fadeInAnimationVariants}
-            initial="initial"
-            whileInView="animate"
-            viewport={{
-              once: true,
-            }}
-            custom={index}
-          >
-            {skill.name}
-            {hoveredSkill === skill.name && (
-              <div className="absolute top-0 left-0 w-full h-full bg-black text-white dark:bg-white flex items-center justify-center dark:text-gray-900 scale-150">
-                {skill.icon}
-              </div>
-            )}
-          </motion.li>
-        ))}
-      </ul>
+      <SectionHeading>My Skills</SectionHeading>
+      
+      {skillsData.map((category, categoryIndex) => (
+        <div key={categoryIndex} className="mb-8">
+          <h3 className="text-xl font-bold mb-4 text-gray-700 dark:text-white">
+            {category.category}
+          </h3>
+          
+          <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+            {category.skills.map((skill, skillIndex) => (
+              <motion.li
+                key={skillIndex}
+                className="relative bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80 group overflow-hidden"
+                onMouseEnter={() => setHoveredSkill(skill.name)}
+                onMouseLeave={() => setHoveredSkill(null)}
+                variants={fadeInAnimationVariants}
+                initial="initial"
+                whileInView="animate"
+                viewport={{
+                  once: true,
+                }}
+                custom={skillIndex}
+              >
+                {skill.name}
+                {hoveredSkill === skill.name && (
+                  <div className="absolute top-0 left-0 w-full h-full bg-black text-white dark:bg-white flex items-center justify-center dark:text-gray-900 scale-150">
+                    {skill.icon}
+                  </div>
+                )}
+              </motion.li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </section>
   );
 }
